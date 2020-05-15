610eabffd78fafadce0232105d1af8f51c0d29ca
Daniel Patricio <daniel.patricio@waproject.com.br>
2020-05-15 14:41:00 +0000

    deploy-server.sh edited online with Bitbucket

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -2,4 +2,4 @@
 echo 'Deploing ' $SERVER_HOST '...'
 mkdir -p ~/.ssh
 (umask  077 ; echo $SSH_KEY | base64 --decode > ~/.ssh/id_rsa)
-ssh ubuntu@$SERVER_HOST 'cd waproject-base && docker-compose pull && docker-compose down && docker-compose up -d --build'
+ssh ubuntu@$SERVER_HOST 'cd'  $SERVER_DIR '&& docker-compose pull && docker-compose down && docker-compose up -d --build'
